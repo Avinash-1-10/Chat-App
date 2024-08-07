ef25139844a2359132210a3d03ef6ad3b588b7ff
Avinash-1-10 <avinashyede110@gmail.com>
2024-08-07 23:19:26 +0530

    add routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 
-const App = () => {
-  return (
-    <div className=''>App</div>
-  )
-}
+// Lazy load the components
+const Auth = lazy(() => import('./pages/auth'));
+const Chat = lazy(() => import('./pages/chat'));
+const Profile = lazy(() => import('./pages/profile'));
 
-export default App
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path='/auth' element={<Auth />} />
+        <Route path='/chat' element={<Chat />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='*' element={<Navigate to='/auth' replace />} />
+      </Routes>
+    </Suspense>
+  </Router>
+);
+
+export default App;
